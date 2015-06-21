b81adb5d93d16cd3ac329e6dfa82bdcac47c043f
Antonio <manentea@gmail.com>
2015-06-21 13:57:21 -0400

    swapped map for our angular map

---
+++ BuyMeCoffee/app/assets/javascripts/map.js
@@ -0,0 +1,24 @@
+var app = angular.module('AngularMaps', ["ngMap", "firebase"]);
+
+var myBase = new Firebase('https://incandescent-torch-1712.firebaseio.com')
+
+app.controller('MapCtrl', function($scope, $firebaseArray){
+
+  var dbMarkers = $firebaseArray(myBase)
+  $scope.markers = dbMarkers;
+
+  $scope.getLocation = function(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position)
+      addMarkers(position)
+    });
+  };
+
+  function addMarkers(position){
+    if(position){
+      $scope.markers.$add({lat: position.coords.latitude + '',lng: position.coords.longitude + ''});
+      $scope.markers.$save();
+    }
+  };
+
+});
