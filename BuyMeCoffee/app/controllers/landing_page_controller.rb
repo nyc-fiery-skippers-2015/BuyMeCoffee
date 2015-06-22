838df0ec7107e5fbc7c9712a5ae8da198eb748e8
dholness2 <DHolness@Dons-MBP.home>
2015-06-22 00:19:04 -0400

    formating page elements

--- BuyMeCoffee/app/controllers/landing_page_controller.rb
+++ BuyMeCoffee/app/controllers/landing_page_controller.rb
@@ -1,6 +1,6 @@
 class LandingPageController < ApplicationController
  def index
-  @users = User.online_users.page(params[:page]).per_page(4)
+  @users = User.online_users.page(params[:page]).per_page(5)
  end
 
 
