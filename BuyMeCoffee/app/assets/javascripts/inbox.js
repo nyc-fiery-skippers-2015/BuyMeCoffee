85647bcd808d5279deac1380c9bee1303fd6757e
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-06-23 10:56:53 -0400

    signup model

--- BuyMeCoffee/app/assets/javascripts/inbox.js
+++ BuyMeCoffee/app/assets/javascripts/inbox.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
   $('#inbox_icon').on('click', inbox);
 
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
+
+
 });
 
 var inbox = function(event){
@@ -17,5 +37,7 @@ var inbox = function(event){
     console.log(error)
   })
 }
+
+
 
 
