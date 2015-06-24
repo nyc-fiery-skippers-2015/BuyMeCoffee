8eaa8703db3eed87791a18dcbd119e933700bb36
Antonio <manentea@gmail.com>
2015-06-24 18:22:28 -0400

    added validation to limit votes to one per person on a mentor

--- BuyMeCoffee/app/models/review.rb
+++ BuyMeCoffee/app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
-  has_one :user
+  has_one :mentor, class_name: 'User'
+
+  validates_uniqueness_of :mentor_id, scope: :author_id, :message => 'You have already rated this user!'
 end
