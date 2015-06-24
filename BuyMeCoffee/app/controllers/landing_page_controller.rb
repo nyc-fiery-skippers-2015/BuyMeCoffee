3dc2cc25a4ee9885afff679397d6c0a74a07b7bb
Antonio <manentea@gmail.com>
2015-06-23 21:46:26 -0400

    adding notification for invites

--- BuyMeCoffee/app/controllers/landing_page_controller.rb
+++ BuyMeCoffee/app/controllers/landing_page_controller.rb
@@ -4,6 +4,7 @@ class LandingPageController < ApplicationController
   @user = User.new
   @categories = Category.all
   @users = User.online_users.page(params[:page]).per_page(5)
+  @invitations = Invitation.any_invites?(session[:user_id])
  end
 
 
--- BuyMeCoffee/app/models/invitation.rb
+++ BuyMeCoffee/app/models/invitation.rb
@@ -1,4 +1,8 @@
 class Invitation < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :mentor, class_name: 'user'
+
+  def self.any_invites?(input)
+    where(mentor_id: input)
+  end
 end
